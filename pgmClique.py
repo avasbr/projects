84d7be580a98ba9ab845092d7826248d6f6ae60e
avasbr <avasbr@avasbr-VirtualBox.(none)>
2013-09-02 21:17:52 -0400

    removed pgmClique.py for now

--- pgmClique.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class Clique():
-	def __init__(self,scope=None,card=None,val=None):
-		self.scope = scope		# full scope of factors
-		self.card = card 		# cardinality of each of the variables
-		self.val = val 			# values for each of assignments
